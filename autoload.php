d68994cfbe4d3bcd69a476ed7beb65d4be8384a6
Berkan KIRAN <berkankiran@hotmail.com>
2018-12-11 05:28:54 +0300

    bump

--- autoload.php
+++ autoload.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once( __DIR__ . '/src/myanimelist/Helper/Builder.php' );
 require_once( __DIR__ . '/src/myanimelist/Helper/Cache.php' );
 require_once( __DIR__ . '/src/myanimelist/Helper/Config.php' );
@@ -10,20 +9,3 @@ require_once( __DIR__ . '/src/myanimelist//Types/Anime.php' );
 require_once( __DIR__ . '/src/myanimelist/Types/Character.php' );
 require_once( __DIR__ . '/src/myanimelist/Types/Manga.php' );
 require_once( __DIR__ . '/src/myanimelist/Types/People.php' );
-
-
-/*
-
-spl_autoload_register(
-	function ( $class )
-	{
-		var_dump( $class ); die;
-		$ds    = DIRECTORY_SEPARATOR;
-		$dir   = __DIR__;
-		$class = str_replace( '\\', $ds, $class );
-		$file  = "{$dir}{$ds}{$class}.php";
-		if ( is_readable( $file ) ) require_once $file;
-	}
-);
-
-*/
